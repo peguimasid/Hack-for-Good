677bf0d06000531c3d78451bbb8e1b123f1d9bd6
Guilhermo Masid <Guilhermo@MacBook-Air-de-Guilhermo.local>
2020-03-31 16:30:01 -0300

    debug: trying to debug merge problem ing github

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,10 @@ routes.get('/news', NewsController.index);
 routes.post('/news', NewsController.store);
 routes.delete('/news/delete/:newsId', NewsController.delete);
 
+routes.get('/fakenews', NewsController.index);
+routes.post('/fakenews', NewsController.store);
+routes.delete('/fakenews/delete/:newsId', NewsController.delete);
+
 routes.post('/sms', SMSController.store);
 
 export default routes;

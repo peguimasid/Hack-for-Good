b01cfbc4986f9bfacaa1ed70f5b324083040c515
Gabriel Henrique <gabrielhenrique_3l@outlook.com>
2020-03-31 22:55:47 -0400

    feature: Authentication | create User | Return token of user

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,11 @@ import { Router } from 'express';
 import NewsController from './controllers/NewsController';
 import SMSController from './controllers/SMSController';
 import CodeValidationController from './controllers/CodeValidationController';
+import SessionController from './controllers/SessionController';
 
 const routes = new Router();
+
+routes.post('/sessions', SessionController.store);
 
 routes.get('/news', NewsController.index);
 routes.post('/news', NewsController.store);

1068c06a2d41bb82d951f379c16fbb7350b42e24
Gabriel <gabrielhenrique_3l@outlook.com>
2020-04-01 13:05:47 -0400

    feature: Create Help | Help list nearby

--- src/controllers/HelpController.js
+++ src/controllers/HelpController.js
@@ -1,23 +1,24 @@
 import * as Yup from 'yup';
-import Knex from 'knex';
 
 import connection from '../database/api';
 
 class HelpController {
   async index(req, res) {
     try {
-      // const { latitude, longitude } = req.query;
-      const latitude = -20.8642561;
-      const longitude = -51.5073472;
+      const { latitude, longitude } = req.query;
 
-      // const haversine = `(6371 * acos(cos(radians(${latitude})) * cos(radians(latitude)) * cos(radians(longitude) - radians(${longitude})) + sin(radians(${latitude})) * sin(radians(latitude))))`;
-      // const haversine = `(((${latitude} - latitude) * (${latitude} - latitude) + (${longitude} - longitude) * (${longitude} - longitude) * 6371))`;
       const helps = await connection('help')
-        .whereRaw(connection.raw(haversine), '<=', 10)
+        .where(
+          connection.raw(
+            `round(((${latitude} - latitude) * (${latitude} - latitude) + (${longitude} - longitude) * (${longitude} - longitude) * 6371),0)`
+          ),
+          '<=',
+          10
+        )
         .select(
           '*',
           connection.raw(
-            `(((${latitude} - latitude) * (${latitude} - latitude) + (${longitude} - longitude) * (${longitude} - longitude) * 6371)) as distance`
+            `round(((${latitude} - latitude) * (${latitude} - latitude) + (${longitude} - longitude) * (${longitude} - longitude) * 6371),0) as distance`
           )
         );
 
